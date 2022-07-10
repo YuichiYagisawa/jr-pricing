e5c6913d9e071d0c8bde26eff5e1d797ae9a1066
Yuichi Yagisawa <yuichi9915@gmail.com>
2022-07-10 14:24:10 +0900

    chore: typescript

--- webpack.config.js
+++ webpack.config.js
@@ -27,3 +27,34 @@ module.exports = {
     ],
   },
 };
+
+
+
+// module.exports = {
+//   // モード値を production に設定すると最適化された状態で、
+//   // development に設定するとソースマップ有効でJSファイルが出力される
+//   mode: "production",
+
+//   module: {
+//     rules: [
+//       {
+//         // 拡張子 .ts の場合
+//         test: /\.ts$/,
+//         // TypeScript をコンパイルする
+//         use: "ts-loader",
+//       },
+//       {
+//         // 対象となるファイルの拡張子
+//         test: /\.(gif|png|jpg)$/,
+//         // 画像をBase64として取り込む
+//         type: "asset/inline",
+//       },
+//     ],
+//   },
+//   // import 文で .ts ファイルを解決するため
+//   resolve: {
+//     extensions: [".ts", ".js"],
+//   },
+//   // ES5(IE11等)向けの指定
+//   target: ["web", "es5"],
+// };
