e340c659e4519a00cbe0843b9ca67ba875710e83
Yuichi Yagisawa <yuichi9915@gmail.com>
2022-07-10 14:12:44 +0900

    add: const

---
+++ src/const.ts
@@ -0,0 +1,11 @@
+export class Const {
+
+  static readonly BASIC_FARE_TO_SHINOSAKA: number = 8910;
+  static readonly BASIC_FARE_TO_HIMEZI: number = 10010;
+  static readonly EXPRESS_FARE_TO_SHINOSAKA: number = 5490;
+  static readonly EXPRESS_FARE_TO_HIMEZI: number = 5920;
+  static readonly FREE_SHEET: number = -530;
+  static readonly DISTANCE_TO_SHINOSAKA: number = 533;
+  static readonly DISTANCE_TO_HIMEZI: number = 644;
+
+}
